3305e5cca9a7c090d494d87287fcca3c78917db7
Sch <junhg0211@gmail.com>
2018-10-21 23:01:04 +0900

    Slo Ogünemay I
    슬로 오귀네마이 원

--- Main.py
+++ Main.py
@@ -33,11 +33,15 @@ class game:
     class display:
         size = slo.slo['display']['size']
         fps = slo.slo['display']['fps']
-        display_fps = fps
+        display_fps = None
 
         pygame.display.set_caption(slo.slo['display']['caption'])
         pygame.display.set_icon(slo.slo['display']['icon'])
     exit = False
+
+    @staticmethod
+    def quit(*_):
+        game.exit = True
 
     window = None
     if slo.slo['display']['fullscreen']:
@@ -53,6 +57,7 @@ class keyboard:
     lalt = False
     ralt = False
     space = False
+    escape = False
 
 class cursor:
     position = pygame.mouse.get_pos()
@@ -72,6 +77,8 @@ def add_object(_obj):
     objects.append(_obj)
 
 class GameObject(object):
+    highlight = None
+
     def tick(self):
         pass
 
@@ -98,7 +105,7 @@ class LockScreen(GameObject):
     date_surface: pygame.Surface
     date_position: tuple
 
-    def __init__(self, colour_text, colour_background, font='./res/font/AppleSDGothicNeoSB.ttf'):
+    def __init__(self, colour_text, colour_background, font=slo.slo['appearance']['font']):
         self.color_text = colour_text
         self.color_background = colour_background
 
@@ -154,7 +161,7 @@ class LockScreen(GameObject):
                         self.x_target = 0
 
             if self.x_target != self.x and game.display.display_fps is not None:
-                self.x += (self.x_target - self.x) / (game.display.display_fps / 6)
+                self.x += (self.x_target - self.x) / (game.display.display_fps / slo.slo['appearance']['motion_speed'])
                 if math.fabs(self.x_target - self.x) < 0.1:
                     self.x = self.x_target
 
@@ -187,31 +194,111 @@ class Shutdown(GameObject):
     gui = 'Shutdown.mode.GUI'
     immediate = 'Shutdown.mode.IMMEDIATE'
 
-    def __init__(self, mode='IMMEDIATE'):
+    class Button(GameObject):
+        text_format = TextFormat(slo.slo['appearance']['font'], 18, color.text)
+
+        def __init__(self, surface, text, action, shutdown):
+            self.surface = surface
+            self.text = text
+            self.command = action[0]
+            self.argument = action[1:]
+            self.shutdown = shutdown
+
+            self.x, self.y = 0, 0
+
+            if self.surface.get_width() != 72:
+                self.surface = pygame.transform.smoothscale(self.surface, (72, self.surface.get_height()))
+            if self.surface.get_height() != 72:
+                self.surface = pygame.transform.smoothscale(self.surface, (self.surface.get_width(), 72))
+
+            self.text_surface = self.text_format.render(self.text)
+            self.text_x, self.text_y = 0, 0
+
+        def tick(self):
+            if self.shutdown.moving:
+                self.x = center(game.display.size[0], 108 * len(self.shutdown.buttons) - 36) + 108 * self.shutdown.buttons.index(self)
+                self.y = center(game.display.size[1], 108) + self.shutdown.y * 2
+
+                self.text_x = center(game.display.size[0], len(self.shutdown.buttons) * 108 - 36) - self.text_surface.get_width() / 2 + 108 * self.shutdown.buttons.index(self) + 36
+                self.text_y = center(game.display.size[1], 108) + 108 - self.text_surface.get_height() + self.shutdown.y * 3
+
+            if cursor.epressed[0]:
+                if self.x <= cursor.position[0] <= self.x + 72 and self.y <= cursor.position[1] <= self.y + 72:
+                    self.command(*self.argument)
+
+        def render(self):
+            game.window.blit(self.surface, (self.x, self.y))
+            game.window.blit(self.text_surface, (self.text_x, self.text_y))
+
+    def __init__(self, mode=gui):
         self.mode = mode
 
+        self.moving = True
+        self.y = -game.display.size[1]
+        self.target_y = 0
+
+        self.background = pygame.Surface(game.display.size)
+
+        self.buttons = []
+        self.buttons.append(self.Button(pygame.image.load('./res/image/shutdown.png'), '시스템 종료', (game.quit,), self))
+
+        GameObject.highlight = Shutdown
+
+    def tick(self):
         if self.mode == self.immediate:
-            game.exit = True
+            game.quit()
             self.destroy()
+
+        if self.moving:
+            self.y += (self.target_y - self.y) / (game.display.display_fps / slo.slo['appearance']['motion_speed'])
+
+        for button in self.buttons:
+            button.tick()
+
+        if keyboard.escape:
+            self.target_y = -game.display.size[1] - 10
+            self.moving = True
+
+        if self.y <= -game.display.size[1] - 1:
+            self.destroy()
+
+    def render(self):
+        game.window.blit(self.background, (0, self.y))
+
+        for button in self.buttons:
+            button.render()
+
+    def destroy(self):
+        GameObject.highlight = None
+        super().destroy()
 
 class Bucker(GameObject):
     class DockItem(GameObject):
-        def __init__(self, surface, action, x, y, dock_bucker):
+        def __init__(self, surface, action, dock_bucker):
             self.surface = surface
             self.command = action[0]
             self.argument = action[1:]
-            self.original_x = x
-            self.original_y = y
             self.dock_bucker = dock_bucker
-            
+
+            self.original_y = 11
+            self.original_x = 20 + len(self.dock_bucker.dock_items) * 92
+
+            if self.surface.get_width() != 72:
+                self.surface = pygame.transform.smoothscale(self.surface, (72, self.surface.get_height()))
+            if self.surface.get_height() != 72:
+                self.surface = pygame.transform.smoothscale(self.surface, (self.surface.get_width(), 72))
+            self.surface.convert()
+
             self.x = self.original_x
             self.y = self.original_y
 
         def tick(self):
             if cursor.epressed[0]:
                 if self.x <= cursor.position[0] <= self.x + self.surface.get_width() and self.y <= cursor.position[1] <= self.y + self.surface.get_height():
-                    self.command(*self.argument)
-                    print('asd')
+                    if self.command == add_object:
+                        self.command(self.argument[0]())
+                    else:
+                        self.command(*self.argument)
 
             self.x = self.original_x + self.dock_bucker.dock_x
             self.y = self.original_y + self.dock_bucker.dock_y
@@ -220,23 +307,24 @@ class Bucker(GameObject):
             game.window.blit(self.surface, (self.x, self.y))
 
     def __init__(self):
-        self.dock_width = game.display.size[0] / 4 * 3
-        self.dock_height = game.display.size[1] / 15
+        # text_format = TextFormat('./res/font/consola.ttf', 72, color.text)
+
+        self.dock_items = []
+        self.dock_items.append(self.DockItem(pygame.image.load('./res/image/shutdown.png'), (add_object, Shutdown), self))
+
+        self.dock_width = len(self.dock_items) * 92 + 20
+        self.dock_height = 92
         self.dock_x = center(game.display.size[0], self.dock_width)
         self.dock_y = game.display.size[1]
         self.dock_y_target = game.display.size[1]
         self.dock_color = color.gray
-
-        text_format = TextFormat('./res/font/consola.ttf', 72, color.text)
-
-        self.dock_items = [self.DockItem(text_format.render('SD'), (add_object, Shutdown()), 10, 10, self)]
 
     def tick(self):
         if game.state != state.lock:
             self.dock_y_target = game.display.size[1] - self.dock_height
 
         if self.dock_y_target != self.dock_y and game.display.display_fps is not None:
-            self.dock_y += (self.dock_y_target - self.dock_y) / (game.display.display_fps / 6)
+            self.dock_y += (self.dock_y_target - self.dock_y) / (game.display.display_fps / slo.slo['appearance']['motion_speed'])
             if math.fabs(self.dock_y_target - self.dock_y) < 0.1:
                 self.dock_y = self.dock_y_target
 
@@ -257,15 +345,18 @@ class Bucker(GameObject):
 class HUD(GameObject):
     state_surface: pygame.Surface
     fps_surface: pygame.Surface
+    objects_surface: pygame.Surface
     text_format = TextFormat('./res/font/consola.ttf', 20, color.text)
 
     def tick(self):
         self.fps_surface = self.text_format.render(str(game.display.display_fps) + ' FPS')
         self.state_surface = self.text_format.render(str(game.state) + ' STATE')
+        self.objects_surface = self.text_format.render(str(len(objects)) + ' OBJECT')
 
     def render(self):
         game.window.blit(self.fps_surface, (0, 0))
         game.window.blit(self.state_surface, (0, 20))
+        game.window.blit(self.objects_surface, (0, 40))
 
 hud = None
 if slo.slo['display']['hud']:
@@ -307,7 +398,7 @@ while not game.exit:
         cursor.pressed = pygame.mouse.get_pressed()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                game.exit = True
+                game.quit()
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RALT:
                     keyboard.ralt = True
@@ -315,9 +406,11 @@ while not game.exit:
                     keyboard.lalt = True
                 elif event.key == pygame.K_F4:
                     if keyboard.lalt or keyboard.ralt:
-                        game.exit = True
+                        game.quit()
                 elif event.key == pygame.K_SPACE:
                     keyboard.space = True
+                elif event.key == pygame.K_ESCAPE:
+                    keyboard.escape = True
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_RALT:
                     keyboard.ralt = False
@@ -325,6 +418,8 @@ while not game.exit:
                     keyboard.lalt = False
                 elif event.key == pygame.K_SPACE:
                     keyboard.space = False
+                elif event.key == pygame.K_ESCAPE:
+                    keyboard.escape = False
 
         for i in range(3):
             cursor.fpressed[i] = not cursor.ppressed[i] and cursor.pressed[i]
@@ -332,7 +427,8 @@ while not game.exit:
 
         game.window.fill(color.background)
         for obj in objects:
-            obj.tick()
+            if type(obj) == GameObject.highlight or GameObject.highlight is None:
+                obj.tick()
         if slo.slo['display']['hud']: hud.tick()
 
         for obj in objects:
